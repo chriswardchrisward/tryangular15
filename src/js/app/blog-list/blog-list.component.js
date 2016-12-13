9e21ad880f315b90d85b80e4b60f14768f847533
Chris Ward <chriswardchrisward@gmail.com>
2016-12-12 19:23:07 -0800

    Moved component html code into an external template

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -2,15 +2,15 @@
 
 angular.module('blogList').
 	component('blogList', {
-		template: "<div class=''><h1 class='new-class'>{{ title }}</h1><button ng-click='someClickTest()'>Click me!</button></div>",
+		templateUrl: '/templates/blog-list.html',
 		controller: function($scope){
 			console.log("hello")
 			$scope.title = 'Hi there'
 			$scope.clicks = 0
 			$scope.someClickTest = function(){
-			console.log("It clicked!")
-			$scope.title = 'Clicked' + ' ' + $scope.clicks + ' ' + 'times'
-			$scope.clicks += 1
+				console.log("It clicked!")
+				$scope.title = 'Clicked' + ' ' + $scope.clicks + ' ' + 'times'
+				$scope.clicks += 1
 		}
 		}
 	});
