54ad26cf9218f004a8c92df75479e13a5b896cc4
Chris Ward <chriswardchrisward@gmail.com>
2016-12-13 11:32:38 -0800

    adding forEach loop for url  routing

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -3,9 +3,9 @@
 angular.module('blogDetail').
 	component('blogDetail', {
 		templateUrl: '/templates/blog-detail.html',
-		controller: function($routeParams, $scope){
+		controller: function($location, $routeParams, $scope){
 			var blogItems = [
-				{title: "Title Number 1", id: 1, description: "This is the first description"},
+				{title: "Title Number 1", id: 1, description: "This is the first description", publishDate: "2016 09 14"},
 				{title: "Title Number 2", id: 2, description: "This is the seccond description"},
 				{title: "Title Number 3", id: 3, description: "This is the third description"},
 				{title: "Title Number 4", id: 4, description: "This is the fourth description"},
@@ -14,6 +14,19 @@ angular.module('blogDetail').
 			
 			// console.log($routeParams.id)
 			$scope.title = "Blog " + $routeParams.id
+			$scope.notFound = true
+			angular.forEach(blogItems, function(post){
+				if (post.id == $routeParams.id){
+					$scope.notFound = false
+					$scope.post = post
+				}
+			})
+
+			if ($scope.notFound) {
+				console.log("Not Found")
+				//change locaiton
+				$location.path("/404")
+			}
 
 	}
 });
