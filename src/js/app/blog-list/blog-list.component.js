61833e61793ba32f4971c6d58efb2aec18dc4b27
Chris Ward <chriswardchrisward@gmail.com>
2016-12-12 19:50:48 -0800

    refocussed to the blog aspect. Created a list of dictionaries to simulate what would be coming from the backend. Iterated through this list in the template to display them using ng-repeat

--- src/js/app/blog-list/blog-list.component.js
+++ src/js/app/blog-list/blog-list.component.js
@@ -4,6 +4,16 @@ angular.module('blogList').
 	component('blogList', {
 		templateUrl: '/templates/blog-list.html',
 		controller: function($scope){
+			var blogItems = [
+				{title: "Title Number 1", id: 1, description: "This is the first description"},
+				{title: "Title Number 2", id: 2, description: "This is the seccond description"},
+				{title: "Title Number 3", id: 3, description: "This is the third description"},
+				{title: "Title Number 4", id: 4, description: "This is the fourth description"},
+				{title: "Title Number 5", id: 5, description: "This is the fif description"},
+			]
+
+			$scope.items = blogItems;
+
 			console.log("hello")
 			$scope.title = 'Hi there'
 			$scope.clicks = 0
