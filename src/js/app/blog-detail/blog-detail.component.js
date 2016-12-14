5cf8648af33cbc23776feded9a15503e79a7524a
Chris Ward <chriswardchrisward@gmail.com>
2016-12-13 20:54:22 -0800

    Added angular adding comment functionality and filtering

--- src/js/app/blog-detail/blog-detail.component.js
+++ src/js/app/blog-detail/blog-detail.component.js
@@ -10,9 +10,24 @@ angular.module('blogDetail').
 					if (post.id == $routeParams.id){
 						$scope.notFound = false
 						$scope.post = post
+						resetReply()
 					}
 				})
 			})
+
+			$scope.addReply = function(){
+				console.log($scope.reply)
+				$scope.post.comments.push($scope.reply)
+
+				resetReply()
+			}
+
+			function resetReply(){
+				$scope.reply = {
+					"id": $scope.post.comments.length + 1,
+					"test": "",
+				}
+			}
 
 			if ($scope.notFound) {
 				console.log("Not Found")
